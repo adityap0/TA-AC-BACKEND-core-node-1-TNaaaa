81c961c29141366191d35d2b1d46012823e926d3
Aditya Pathak <mynewchilly@gmail.com>
2021-06-26 18:18:46 +0530

    re

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,13 +1,19 @@
-let { readFile, readFileSync } = require("fs");
-let path = require("path");
-let filepath = path.join(__dirname, "content.md");
-readFile("content.md", "utf8", (error, file) => {
-  console.log(file);
-});
+// let { readFile, readFileSync } = require("fs");
+// let path = require("path");
+// let filepath = path.join(__dirname, "content.md");
+// readFile("content.md", "utf8", (error, file) => {
+//   console.log(file);
+// });
 
-var final = readFileSync("content.md", "utf8");
-console.log(final);
+// var final = readFileSync("content.md", "utf8");
+// console.log(final);
 
-var buff1 = Buffer.alloc(10);
-buff1.write(`Welcome to Buffer`);
-console.log(buff1);
+// var buff1 = Buffer.alloc(10);
+// buff1.write(`Welcome to Buffer`);
+// console.log(buff1);
+
+let urlString = `https://www.instagram.com`;
+let url = require("url");
+
+var parsedUrl = url.parse(urlString, true);
+console.log(parsedUrl);
